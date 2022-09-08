c81369f3b31986ff382b6d6165f24acd02324528
diana <dcecurskaya@gmail.com>
2022-09-08 18:15:48 +0300

    first

---
+++ src/telran/time/BarMitzvaAdjuster.java
@@ -0,0 +1,14 @@
+package telran.time;
+
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+
+public class BarMitzvaAdjuster implements TemporalAdjuster {
+
+	@Override
+	public Temporal adjustInto(Temporal temporal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
